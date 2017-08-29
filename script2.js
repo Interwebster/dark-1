e3097cf2e1e07e484d2648c15cf04767cab526af
Caio Caly <30990504+caiocaly@users.noreply.github.com>
2017-08-29 12:25:49 -0300

    Atualizei pra ficar igual os meus

--- script2.js
+++ script2.js
@@ -1,22 +1,27 @@
 var quarto = {
-	padrao: "Testando isso",
-	apagado: "tudo escuro kkk"
+	visitas: 0,
+	primeira: `<p> É o seu quarto. Ele está bem arrumadinho, até a cama está feita. </p>
+	<p> É pequeno, mas aconchegante...	Olhar pra ele assim com atenção faz você lembrar de como gosta dele...<br> Mas também parece que tem alguma coisa errada, 
+	mesmo que você não saiba exatamente o quê é...</p>`,
+	decima: `<p> Quanto mais tempo cê passa aqui, mais estranho tudo parece... </p>`
 }
 
 printContent = function (targetRoom, targetState, printType){
 	if (printType == 'add') {
+		document.getElementById('main').innerHTML += "<p>" + targetRoom[targetState] + "</p>";
 
-	  document.getElementById('main').innerHTML += "<p>" + targetRoom[targetState] + "</p>";
-
-
-	
 	} else if (printType == 'replace') {
-	  document.getElementById('main').innerHTML = "<p>" + targetRoom[targetState] + "</p>";
+		document.getElementById('main').innerHTML = "<p>" + targetRoom[targetState] + "</p>";
 	}
 }
 
-testContent = function () {
-	//document.getElementById('teste').innerHTML = 'Oi';
-	 document.getElementById('main').innerHTML = quarto.padrao;
-}
+Quarto = function () {
+		printContent (quarto, 'primeira', 'replace' );
 
+		if (quarto.visitas > 3) {
+		printContent (quarto, 'decima', 'add');
+		}
+	
+
+	quarto.visitas ++;
+}
